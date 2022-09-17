6e6dad98c7f683cbeb403a9e4a704c038ab61c68
Agustín <agustinngarrone@hotmail.com>
2022-09-17 19:02:57 -0300

    added other bot.

--- index.js
+++ index.js
@@ -4,44 +4,69 @@ const app = express()
 require('dotenv').config()
 const Twit = require('twit')
 
-var T = new Twit({
+var T1 = new Twit({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token: process.env.access_token,
   access_token_secret:process.env.access_token_secret
 })
+var T2 = new Twit({
+  consumer_key: process.env.consumer_key2,
+  consumer_secret: process.env.consumer_secret2,
+  access_token: process.env.access_token2,
+  access_token_secret:process.env.access_token_secret2
+})
 
 
-var allTweet;
+var allTweet1;
+var allTweet2;
 
   
 
-function tweetToDto(tweet) {
+    function tweetFromAccountOne(tweet) {
+      if (tweet.in_reply_to_screen_name != null) {
+          let res = {
+              status: '⚡ Hey, bro check @cryptocupqatar first crypto tournament based in qatar world cup 2022!⚡ #qatar2022 #worldcup2022 #p2e'  ,
+              in_reply_to_status_id: '' + tweet.id_str ,
+              in_reply_to_user_id:'' + tweet.user.id,
+              in_reply_to_screen_name:''+ tweet.user.screen_name,
+              auto_populate_reply_metadata: true
+            };
+          T1.post('statuses/update', res, function(err, data, response) {
+              if (err) console.log(err)
+          })  
+        }
+    }   
 
-
-    if (tweet.in_reply_to_screen_name != null) {
-        var res = {
-            status: '⚡ Hey, bro check @cryptocupqatar first crypto tournament based in qatar world cup 2022!⚡ #playtoearn #worldcup2022 #p2e'  ,
-            in_reply_to_status_id: '' + tweet.id_str ,
-            in_reply_to_user_id:'' + tweet.user.id,
-            in_reply_to_screen_name:''+ tweet.user.screen_name,
-            auto_populate_reply_metadata: true
-          };
-
-        T.post('statuses/update', res, function(err, data, response) {
-            if (err) console.log(err)
-    })  
-    }}  
+    function tweetFromAccountTwo(tweet) {
+      if (tweet.in_reply_to_screen_name != null) {
+          let res = {
+              status: '⚡ Hey, bro check @cryptocupqatar first crypto tournament based in qatar world cup 2022!⚡ #qatar2022 #worldcup2022 #p2e'  ,
+              in_reply_to_status_id: '' + tweet.id_str ,
+              in_reply_to_user_id:'' + tweet.user.id,
+              in_reply_to_screen_name:''+ tweet.user.screen_name,
+              auto_populate_reply_metadata: true
+            };
+          T2.post('statuses/update', res, function(err, data, response) {
+              if (err) console.log(err)
+          })  
+        }
+    }   
     
     function loopGetTweets() {
       console.log("ejecutando loopget");
-      T.get('search/tweets', { q: '#nft since:2022-07-11', count: 500 },async function(err, data, response) {
-        allTweet = data.statuses.map(tweet => tweetToDto(tweet))
-        console.log("ejecutando");
+      T1.get('search/tweets', { q: '#nft since:2022-07-11', count: 500 },async function(err, data, response) {
+        allTweet1 = data.statuses.map(tweet => tweetFromAccountOne(tweet))
       })
+
+      T2.get('search/tweets', { q: '#playtoearn since:2022-07-11', count: 500 },async function(err, data, response) {
+        allTweet2 = data.statuses.map(tweet => tweetFromAccountTwo(tweet))
+      })
+
       setTimeout(() => {
         loopGetTweets()
       }, 1800000);
+      
     }
 
 loopGetTweets()
